e44944924a5b6ca1de3364847cb458c4d6976084
Yanko Mirov <45804656+tranculent@users.noreply.github.com>
2021-03-09 12:20:35 +0200

    Fixed the reportError function

--- Generate.java
+++ Generate.java
@@ -1,11 +1,13 @@
 // @Author: Yanko Mirov (37513192)
+
+import java.util.*;
 public class Generate extends AbstractGenerate {
+
+
     public Generate() {
-        System.out.println("Creating a generate class..");
     }
 
     public void reportError(Token token, String explanatoryMessage) throws CompilationException {
-        throw new CompilationException("Exop");
-    }
-    
+        throw new CompilationException(explanatoryMessage);
+    } 
 }
